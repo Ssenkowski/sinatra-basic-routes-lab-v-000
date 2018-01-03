43b1e19a7eb0568842df7c3f505a26aa4bf6dee4
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 22:41:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,15 +8,18 @@ class App < Sinatra::Base
   get '/name' do
     @name = Name.all
     erb :'name/index'
+    resp.write "My name is \_\_"
   end
 
   get '/hometown' do
     @hometown = Hometown.all
     erb :'hometown/index'
+    resp.write "My hometown is \_\_"
   end
 
   get '/favorite-song' do
     @favorite_song = Favorite_song.all
     erb :'favorite-song/index'
+    resp.write "My favorite-song is \_\_"
   end
 end
