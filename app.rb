4c97384467ce7a0b03160efbc09c8f7c075cdc06
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 22:45:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,15 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/name' do
+
+if get '/name' do
     resp.write "My name is \_\_"
   end
 
-  get '/hometown' do
+elsif  get '/hometown' do
     resp.write "My hometown is \_\_"
   end
 
-  get '/favorite-song' do
+else  get '/favorite-song' do
     resp.write "My favorite song is \_\_"
   end
 end
