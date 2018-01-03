d5accaf7e089cbdfbd8d7a3e043aeec375147d50
Steven Senkowski <scsenkowski@gmail.com>
2018-01-03 22:39:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,4 +4,17 @@ class App < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
+
+  get '/name' do
+    @name = Name.all
+    erb :'name/index'
+  end
+
+  get '/hometown' do
+    
+  end
+
+  get '/favorite-song' do
+    
+  end
 end
